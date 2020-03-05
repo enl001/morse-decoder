3a65f98058acce8747727615c750b33112730bf0
enl001 <enliven001@mail.ru>
2020-03-06 00:05:05 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result=[];
+    let i=0;
+    let length=10;
+    arr = expr.split('**********');    
+    for(let a of arr)
+    {
+        let start=0;
+        let end=length;        
+        while(start<=a.length-1){
+            let l = a.substring(start,end);            
+            let index = l.indexOf('1');
+            result[i++]= parse(l.substring(index));            
+            start=end;
+            end+=length;            
+        }
+        result[i++] = ' ';
+    }
+    return (result.join('').trim());
 }
+
+function parse(word){
+    let start=0;
+    let end=2;        
+    let result ='';  
+    while(start<=word.length-1){
+      let s = word.substring(start,end);
+      switch(s)
+      {
+        case '10':
+        result+='.';
+        break;
+        case '11':
+        result+='-';
+        break;
+      }
+      start=end;
+      end+=2;
+    }    
+    return MORSE_TABLE[result];
+  }
 
 module.exports = {
     decode
